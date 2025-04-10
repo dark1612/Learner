1167da6b9509defb327bbe6ca3235f94b27766f1
dark1612 <your-email@example.com>
2025-04-11 00:52:51 +0530

    Fix merge conflict in server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-<<<<<<< HEAD
+
 const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
@@ -10,7 +10,7 @@ const studentViewCourseRoutes = require("./routes/student-routes/course-routes")
 const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
 const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
 const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
-=======
+
 
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
@@ -21,7 +21,7 @@ const studentCourseprogressRoutes = require("./routes/student-routes/course-prog
 
 
 
->>>>>>> db7c6e42a7c25664fff6a045b940aacacc517815
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -44,7 +44,7 @@ mongoose
   .catch((e) => console.log(e));
 
 //routes configuration
-<<<<<<< HEAD
+
 app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
@@ -52,7 +52,7 @@ app.use("/student/course", studentViewCourseRoutes);
 app.use("/student/order", studentViewOrderRoutes);
 app.use("/student/courses-bought", studentCoursesRoutes);
 app.use("/student/course-progress", studentCourseProgressRoutes);
-=======
+
 app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course",studentViewCourseRoutes);
@@ -60,7 +60,7 @@ app.use("/student/courses-bought", studentCoursesRoutes);
 app.use("/student/course-progress", studentCourseprogressRoutes);
 
 
->>>>>>> db7c6e42a7c25664fff6a045b940aacacc517815
+
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
@@ -72,8 +72,8 @@ app.use((err, req, res, next) => {
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> db7c6e42a7c25664fff6a045b940aacacc517815
+
+
+
