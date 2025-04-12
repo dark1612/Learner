d8cee8b19848edd7ba2f4cd0819bd8b2a3da66bd
dark1612 <your-email@example.com>
2025-04-13 01:02:53 +0530

    Updated API base URL and .env

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  withCredentials: true, // optional if you're using cookies
 });
 
 axiosInstance.interceptors.request.use(
